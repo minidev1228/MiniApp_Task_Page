2297808ac4b73c208616469284949ba09d6d33b5
minidev1228 <mightybluedev@gmail.com>
2024-11-11 03:33:35 +0900

    Add link

--- src/pages/Landing.page.tsx
+++ src/pages/Landing.page.tsx
@@ -2,7 +2,6 @@ import Layout from "../Layouts/index";
 import TaskCard from "../components/TaskCard";
 
 const RocketImg = "https://img.icons8.com/?size=100&id=80ArDaExWQAI&format=png&color=3a8ef0"
-import LoginImg from "../assets/bonus.png"
 import TelegramImg from "../assets/telegram.png"
 import DiscordImg from "../assets/discord.png"
 import FaceBookImg from "../assets/facebook.png"
@@ -20,12 +19,12 @@ const LandingPage = () =>{
                 <h1 className=" mt-5 text-2xl font-bold mb-5">Earn more coins</h1>
                 <div style={{height:"calc(100vh - 223px)"}} className="w-11/12 overflow-y-scroll">
                     <TaskCard title={"Join our Telegram channel"} bonus={100} img={TelegramImg} link={"https://t.me/hamburg_english"} />
-                    <TaskCard title={"Join our Discord"} bonus={100} img={DiscordImg} />
-                    <TaskCard title={"Follow us on facebook"} bonus={100} img={FaceBookImg} />
-                    <TaskCard title={"Follow us on instgram"} bonus={100} img={InstagramImg} />
-                    <TaskCard title={"Follow us on linkden"} bonus={100} img={LinkdenImg} />
-                    <TaskCard title={"Follow us on twiter"} bonus={100} img={XImg} />
-                    <TaskCard title={"Subscribe youtube channel"} bonus={100} img={YoutubeImg} />
+                    <TaskCard title={"Join our Discord"} bonus={100} img={DiscordImg} link={"https://t.me/hamburg_english"} />
+                    <TaskCard title={"Follow us on facebook"} bonus={100} img={FaceBookImg} link={"https://t.me/hamburg_english"} />
+                    <TaskCard title={"Follow us on instgram"} bonus={100} img={InstagramImg} link={"https://t.me/hamburg_english"} />
+                    <TaskCard title={"Follow us on linkden"} bonus={100} img={LinkdenImg} link={"https://t.me/hamburg_english"} />
+                    <TaskCard title={"Follow us on twiter"} bonus={100} img={XImg} link={"https://t.me/hamburg_english"} />
+                    <TaskCard title={"Subscribe youtube channel"} bonus={100} img={YoutubeImg} link={"https://t.me/hamburg_english"} />
                 </div>
             </div>
         </Layout>
