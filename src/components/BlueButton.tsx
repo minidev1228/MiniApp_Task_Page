155294a53513c7e6eb19d7ad80d3ac267521a366
minidev1228 <mightybluedev@gmail.com>
2024-11-11 04:06:43 +0900

    Add loading to button

--- src/components/BlueButton.tsx
+++ src/components/BlueButton.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React , {useState} from "react"
 
 type Props = {
     content: string;
@@ -8,14 +8,18 @@ type Props = {
 
 const BlueButton: React.FC<Props> = ({content, onClickHandler}) =>{
 
-    const click = () =>{
+    const [isLoading, setIsLoading] = useState(false);
 
+    const click = () =>{
+        setIsLoading(true);
         onClickHandler();
     }
 
     return (
-        <button onClick={()=>{click()}} className=" bg-blue text-black text-sm h-full pt-1 pb-1 pr-3 pl-3 rounded-md w-full">
-            {content}
+        <button onClick={()=>{click()}} className=" bg-blue text-black text-sm h-full pt-1 pb-1 pr-3 pl-3 rounded-md w-full flex justify-center items-center">
+            {
+                isLoading ? <span className="loader" style={{height:"15px", width:"15px"}}></span> : <p>{content}</p>
+            } 
         </button>
     )
 }
